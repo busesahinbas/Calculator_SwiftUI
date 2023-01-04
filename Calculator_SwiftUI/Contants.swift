ef2614cbfab15fe94230fae9ef6de7cd4ff38438
busesahinbas <buse.sahinbas98@gmail.com>
2023-01-04 20:10:35 +0300

    bug fixed

---
+++ Calculator_SwiftUI/Contants.swift
@@ -0,0 +1,23 @@
+//
+//  Contants.swift
+//  Calculator_SwiftUI
+//
+//  Created by Buse Şahinbaş on 3.01.2023.
+//
+
+import Foundation
+import SwiftUI
+
+internal struct Constants {
+    
+    static let smallButtonWidht = (UIScreen.main.bounds.size.width - (5*12)) / 4
+    static let largeButtonWidht = smallButtonWidht * 2 + 12
+    static let heightButton = smallButtonWidht
+    static let heightFrame = UIScreen.main.bounds.size.height * 0.1
+    static let heightText = UIScreen.main.bounds.size.height * 0.32
+    static let darkGrey = Color(red: 0.3137254901960784, green: 0.3137254901960784, blue: 0.3137254901960784)
+    static let lightGrey = Color(red: 0.8313725490196079, green: 0.8313725490196079, blue: 0.8235294117647058)
+    static let buttonPadding = UIScreen.main.bounds.size.width / 100 * 3
+    
+}
+
--- Calculator_SwiftUI/ContentView.swift
+++ Calculator_SwiftUI/ContentView.swift
@@ -8,19 +8,207 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack {
+                Spacer()
+                Text("0")
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .font(.system(size: 85))
+            }.frame(height: Constants.heightText)
+            
+            HStack {
+                Button("AC") {
+                    print("Button tapped!")
+                }
+                .frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.lightGrey)
+                    .clipShape(Circle())
+                    .font(.system(size: 32))
+                
+                Button("+/-") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.lightGrey)
+                    .clipShape(Circle())
+                    .font(.system(size: 32))
+                
+                Button("%") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.lightGrey)
+                    .clipShape(Circle())
+                    .font(.system(size: 32))
+
+                Button("÷") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                 .foregroundColor(Color.white)
+                 .background(Color.orange)
+                 .clipShape(Circle())
+                 .font(.system(size: 32))
+                
+            }.frame(height: Constants.heightFrame)
+                .padding(6)
+
+        
+            HStack {
+                Button("7") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("8") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("9") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("x") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                 .foregroundColor(Color.white)
+                 .background(Color.orange)
+                 .font(.system(size: 32))
+                 .clipShape(Circle())
+
+            }.frame(height: Constants.heightFrame)
+                .padding(6)
+
+            
+            HStack {
+                Button("4") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("5") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("6") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("-") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                 .foregroundColor(Color.white)
+                 .background(Color.orange)
+                 .font(.system(size: 32))
+                 .clipShape(Circle())
+
+            }.frame(height: Constants.heightFrame)
+                .padding(6)
+
+           
+            HStack {
+                Button("1") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("2") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("3") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("+") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                 .foregroundColor(Color.white)
+                 .background(Color.orange)
+                 .font(.system(size: 32))
+                 .clipShape(Circle())
+
+            }.frame(height: Constants.heightFrame)
+                .padding(Constants.buttonPadding)
+
+          
+            HStack (){
+                Button() {
+                    print("Button tapped!")
+                }label: {
+                    Text("0").padding(30)
+                }.frame(width: Constants.largeButtonWidht, height: Constants.heightButton, alignment:Alignment.leading)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Capsule())
+                    //.cornerRadius((UIScreen.main.bounds.size.width - (4*12)) / 2)
+                    
+                Button(",") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                    .foregroundColor(Color.white)
+                    .background(Constants.darkGrey)
+                    .font(.system(size: 32))
+                    .clipShape(Circle())
+                
+                Button("=") {
+                    print("Button tapped!")
+                }.frame(width: Constants.smallButtonWidht, height: Constants.heightButton)
+                 .foregroundColor(Color.white)
+                 .background(Color.orange)
+                 .font(.system(size: 32))
+                 .clipShape(Circle())
+
+            }.frame(height: Constants.heightFrame)
+                .padding(.bottom, Constants.buttonPadding)
+        
+        }.background(Color.black)
     }
+   
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+       //ContentView().previewDevice("iPhone SE (2nd generation)")
         ContentView()
     }
 }
